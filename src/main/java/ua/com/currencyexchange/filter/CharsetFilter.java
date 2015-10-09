68e9cb825fe9337f9021cf202a9c7002ec4c677b
Irochka <afanasievaiv@ya.ru>
2015-10-09 16:17:29 +0300

    encoding!

--- src/main/java/ua/com/currencyexchange/filter/CharsetFilter.java
+++ src/main/java/ua/com/currencyexchange/filter/CharsetFilter.java
@@ -27,7 +27,16 @@ public class CharsetFilter implements Filter
  	ServletResponse response, FilterChain next)
  throws IOException, ServletException
  {
-  request.setCharacterEncoding(encoding);
+  // Respect the client-specified character encoding
+    // (see HTTP specification section 3.4.1)
+    if(null == request.getCharacterEncoding())
+      request.setCharacterEncoding(encoding);
+
+    /**
+    * Set the default response content type and encoding
+   */
+  response.setContentType("text/html; charset=UTF-8");
+  response.setCharacterEncoding("UTF-8");
   next.doFilter(request, response);
  }
  
