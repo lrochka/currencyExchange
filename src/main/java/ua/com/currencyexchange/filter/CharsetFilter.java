5d2fc75ad49e4cf03398dc115f85c901b1cf999c
Irochka <afanasievaiv@ya.ru>
2015-10-10 21:57:24 +0300

    filtersgit statusgit status

--- src/main/java/ua/com/currencyexchange/filter/CharsetFilter.java
+++ src/main/java/ua/com/currencyexchange/filter/CharsetFilter.java
@@ -18,7 +18,7 @@ public class CharsetFilter implements Filter
  
  public void init(FilterConfig config) throws ServletException
  {
-  encoding = config.getInitParameter("encoding");
+  //encoding = config.getInitParameter("encoding");
  
   if( encoding==null ) encoding="UTF-8";
  }
