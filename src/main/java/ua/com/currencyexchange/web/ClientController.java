da808a1772661ffc46d49ff27a47477f71009c68
Irochka <afanasievaiv@ya.ru>
2015-10-11 16:24:50 +0300

    encoding utf-8

--- src/main/java/ua/com/currencyexchange/web/ClientController.java
+++ src/main/java/ua/com/currencyexchange/web/ClientController.java
@@ -96,7 +96,13 @@ public class ClientController {
 		ClientForm clientf = new ClientForm();
 		
 		clientf.setCompany(client.getCompany().getId());
+		try{
 		clientf.setName(client.getName());
+		}catch(Exception e)
+		{
+			model.addAttribute("error", e.getMessage());
+			return "accessdenied";
+		}
 		clientf.setPhone(client.getPhone());
 		clientf.setAddContact(client.getAddContact());
 		
