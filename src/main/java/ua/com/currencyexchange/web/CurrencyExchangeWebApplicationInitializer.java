dd70731142a51895e894581410a1bb63b3a81bb4
Irochka <afanasievaiv@ya.ru>
2015-10-14 00:57:06 +0300

    register filter in java class

---
+++ src/main/java/ua/com/currencyexchange/web/CurrencyExchangeWebApplicationInitializer.java
@@ -0,0 +1,29 @@
+/* 
+ * Copyright (c) 2015
+ */
+package ua.com.currencyexchange.web;
+
+import java.util.Set;
+
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+public class CurrencyExchangeWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        setActiveProfile();
+        
+		charsetFilter = servletContext.addFilter("setCharacterEncodingFilter",
+				new CharacterEncodingFilter());
+		charsetFilter.setInitParameter("encoding", "UTF-8");
+		charsetFilter.setInitParameter("forceEncoding", "true");
+		charsetFilter.addMappingForUrlPatterns(null, false, "/*");
+		
+        super.onStartup(servletContext);
+    }
+}
